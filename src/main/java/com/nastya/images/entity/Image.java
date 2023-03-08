61589bcc5a00daaa123c05ab7f27c24f921a3963
MichaelZalogin <zaloginmiha@gmail.com>
2023-03-09 02:25:41 +0300

    Fix Entity

--- src/main/java/com/nastya/images/entity/Image.java
+++ src/main/java/com/nastya/images/entity/Image.java
@@ -2,8 +2,6 @@ package com.nastya.images.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
-import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,12 +11,6 @@ import java.util.UUID;
 @Entity
 @Table(name = "image")
 public class Image extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(name = "id")
-    private UUID id;
 
     @Column(name = "path")
     private String path;
